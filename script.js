c579307f9d722417c2b7fe91e5e4aad36820b303
Vikram pratap <122038154+vikrampratap2002@users.noreply.github.com>
2026-03-13 10:55:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+let sounds = [
+"applause",
+"boo",
+"gasp",
+"tada",
+"victory",
+"wrong"
+]
+
+let currentAudio = null
+
+document.querySelectorAll(".btn").forEach((btn,index)=>{
+
+btn.addEventListener("click",()=>{
+
+if(currentAudio){
+currentAudio.pause()
+}
+
+currentAudio = new Audio("sounds/"+sounds[index]+".mp3")
+
+currentAudio.play()
+
+})
+
+})
+
+document.querySelector(".stop").addEventListener("click",()=>{
+
+if(currentAudio){
+currentAudio.pause()
+currentAudio.currentTime = 0
+}
+
+})
